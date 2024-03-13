3086591eef0b08fa9009d669af33df88cbe989ab
victorpreston <prestonvictor25@gmail.com>
2024-03-13 13:28:48 +0300

    [v1]: Notely

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,20 @@ import { ThemeToggle } from "./components/Themetoggle";
 
 export default function Home() {
   return (
-    <div>
-      <h1> Notely is here with us</h1>
-    </div>
+    <section className="flex items-center justify-center bg-background h-[90vh]">
+      <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
+        <div className="max-w-3xl mx-auto text-center">
+          <div>
+            <span className="w-auto px-6 py-3 rounded-full bg-secondary">
+              <span className="text-sm font-medium text-primary">
+                Sort your notes by category, date, or title.
+              </span>
+            </span>
+          </div>
+        </div>
+          
+      </div>
+
+    </section>
   );
 }
