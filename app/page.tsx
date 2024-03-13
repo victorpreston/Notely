9a21f231f5db2c0cdd8cbf623e9c1b5f194a7932
victorpreston <prestonvictor25@gmail.com>
2024-03-13 14:54:35 +0300

    [v1]: Notely

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 import { ThemeToggle } from "./components/Themetoggle";
+import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { Button } from "@/components/ui/button";
+import Link from 'next/link';
+import { FaGithub } from 'react-icons/fa';
 
 export default function Home() {
   return (
@@ -12,6 +16,27 @@ export default function Home() {
                 Sort your notes by category, date, or title.
               </span>
             </span>
+            <h1 className="mt-8 text-4xl font-extrabold tracking-tight lg:text-6xl">
+              Create Your Notes 
+            </h1>
+            <p className="mx-w-xl mx-auto mt-8 text-base lg:text-xl text-secondary-foreground ">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Sed ut purus neque. Phasellus sit amet ante ut velit aliquet tincidunt. 
+            Donec feugiat eros vel nisi sagittis
+            </p>
+          </div>
+          <div className="flex justify-center max-w-sm mx-auto mt-10 space-x-4">
+          <RegisterLink>
+              <Button size="lg" className="w-full">
+                Get Started
+              </Button>
+            </RegisterLink>
+          <Link href="https://github.com/victorpreston/Notely" passHref>
+            <Button size="lg" className="w-full flex items-center justify-center">
+              <FaGithub className="mr-2" />
+              Star
+            </Button>
+          </Link>
           </div>
         </div>
           
