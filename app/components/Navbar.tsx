9ec23364fde554078a746cdb1dff1ce20d6e1720
victorpreston <prestonvictor25@gmail.com>
2024-03-13 13:13:20 +0300

    [v1]: Notely

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link";
 import { ThemeToggle } from "./Themetoggle";
 import { Button } from "@/components/ui/button";
+import {
+    RegisterLink,
+    LoginLink,
+    LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 
 
-export function Navbar() {
+export async function Navbar() {
+    const { isAuthenticated } = getKindeServerSession();
     return (
         <nav className="border-b bg-background h-[10vh] flex items-center">
             <div className="container flex items-centre justify-between">
@@ -15,10 +22,21 @@ export function Navbar() {
             </div>
             <div className="flex items-center gap-x-5">
                 <ThemeToggle />
-                <div className="flex items-center gap-x-5">
+                {(await isAuthenticated()) ? (
+                    <LogoutLink>
+                        <Button className="mr-3" >Log Out</Button>
+                    </LogoutLink>
+                ) : (
+                    <div className="flex items-center gap-x-5">
+                    <LoginLink>
                     <Button> SignIn</Button>
+                    </LoginLink>
+                    
+                    <RegisterLink>
                     <Button className="mr-3" variant="secondary"> SignUp</Button>
+                    </RegisterLink>
                 </div>
+                )}  
             </div>
 
         </nav>
