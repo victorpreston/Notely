cae8f653f323a243b7f3c38ed9b335fe804f2e77
victorpreston <prestonvictor25@gmail.com>
2024-03-17 04:02:41 +0300

    + Features Page

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import { FaGithub } from 'react-icons/fa';
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import Features from "./pages/Features";
 
 
 export default async function Home() {
@@ -15,7 +16,7 @@ export default async function Home() {
     return redirect("/dashboard");
   }
   return (
-    <section className="flex items-center justify-center bg-background h-[90vh]">
+    <><section className="flex items-center justify-center bg-background h-[90vh]">
       <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
         <div className="max-w-3xl mx-auto text-center">
           <div>
@@ -25,38 +26,41 @@ export default async function Home() {
               </span>
             </span>
             <h1 className="mt-8 text-4xl  lg:text-6xl text-balance bg-gradient-to-br  from-gray-900 via-gray-800 to-gray-400 bg-clip-text text-center font-heading text-[40px] font-bold leading-tight tracking-[-0.02em] text-transparent  drop-shadow-sm duration-300 ease-linear [word-spacing:theme(spacing.1)] md:text-7xl md:leading-[5rem] dark:bg-gradient-to-br dark:from-gray-100 dark:to-gray-900">
-              Create Your Notes 
+              Create Your Notes
             </h1>
             <p className="mx-w-xl mx-auto mt-8 text-base lg:text-xl text-secondary-foreground ">
-            With Notely, you can easily create, save, edit and delete notes at your convenience.
-            Our platform offers a user friendly interface that makes note management a breeze.
+              With Notely, you can easily create, save, edit and delete notes at your convenience.
+              Our platform offers a user friendly interface that makes note management a breeze.
             </p>
           </div>
           <div className="flex justify-center max-w-sm mx-auto mt-10 space-x-4">
-          <RegisterLink>
+            <RegisterLink>
               <Button size="lg" className="w-full">
                 Get Started
               </Button>
             </RegisterLink>
-            <Link 
-              href="https://github.com/victorpreston/Notely" 
+            <Link
+              href="https://github.com/victorpreston/Notely"
               passHref
               target="_blank"
               rel="noreferrer"
             >
-              <Button 
-                size="lg" 
+              <Button
+                size="lg"
                 className="w-full"
               >
                 <FaGithub className="mr-2" />
-                Star 
+                GitHub
               </Button>
             </Link>
           </div>
         </div>
-          
+
       </div>
 
     </section>
+    <section>
+      <Features />
+    </section></>
   );
 }
---
+++ app/pages/Features.tsx
@@ -0,0 +1,102 @@
+export default function Features() {
+    return (
+        <><section
+            id="features"
+            className="container space-y-6 bg-slate-50 py-8 md:py-12 lg:py-24 dark:bg-transparent">
+            <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+                <h2 className="mt-8 text-4xl  lg:text-6xl text-balance bg-gradient-to-br  from-gray-900 via-gray-800 to-gray-400 bg-clip-text text-center font-heading text-[40px] font-bold leading-tight tracking-[-0.02em] text-transparent  drop-shadow-sm duration-300 ease-linear [word-spacing:theme(spacing.1)] md:text-7xl md:leading-[5rem] dark:bg-gradient-to-br dark:from-gray-100 dark:to-gray-900">
+                    Features
+                </h2>
+                <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+                    This project is an experiment to see how a modern app, with features
+                    like auth, subscriptions, API routes, and static pages would work in
+                    Next.js 14 app dir.
+                </p>
+            </div>
+            <div className="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
+                <div className="relative overflow-hidden rounded-lg border bg-background p-2">
+                    <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-shield-plus">
+                    <path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"/>
+                    <path d="M9 12h6"/>
+                    <path d="M12 9v6"/>
+                    </svg>
+                        <div className="space-y-2">
+                            <h3 className="font-bold">Authentication</h3>
+                            <p className="text-sm text-muted-foreground">
+                                App dir, Routing, Layouts, Loading UI and API routes.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="relative overflow-hidden rounded-lg border bg-background p-2">
+                    <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-layout-dashboard">
+                    <rect width="7" height="9" x="3" y="3" rx="1"/><rect width="7" height="5" x="14" y="3" rx="1"/><rect width="7" height="9" x="14" y="12" rx="1"/><rect width="7" height="5" x="3" y="16" rx="1"/></svg>
+                        <div className="space-y-2">
+                            <h3 className="font-bold">Dashboard</h3>
+                            <p className="text-sm text-muted-foreground">
+                                Server and Client Components. Use hook.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="relative overflow-hidden rounded-lg border bg-background p-2">
+                    <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-settings">
+                    <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"/>
+                    <circle cx="12" cy="12" r="3"/></svg>
+                        <div className="space-y-2">
+                            <h3 className="font-bold">Settings</h3>
+                            <p className="text-sm text-muted-foreground">
+                                Postgres basic support added. Use node pg.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="relative overflow-hidden rounded-lg border bg-background p-2">
+                    <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-bookmark">
+                        <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"/>
+                    </svg>
+                        <div className="space-y-2">
+                            <h3 className="font-bold">Bookmarks</h3>
+                            <p className="text-sm text-muted-foreground">
+                                UI components built using Radix UI and styled with Tailwind
+                                CSS.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="relative overflow-hidden rounded-lg border bg-background p-2">
+                    <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-arrow-down-wide-narrow">
+                        <path d="m3 16 4 4 4-4"/><path d="M7 20V4"/><path d="M11 4h10"/><path d="M11 8h7"/>
+                    <path d="M11 12h4"/></svg>
+                        <div className="space-y-2">
+                            <h3 className="font-bold">Sorting</h3>
+                            <p className="text-sm text-muted-foreground">
+                                Authentication using NextAuth.js and middlewares.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="relative overflow-hidden rounded-lg border bg-background p-2">
+                    <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-folder-kanban"><path d="M4 20h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.93a2 2 0 0 1-1.66-.9l-.82-1.2A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13c0 1.1.9 2 2 2Z"/>
+                    <path d="M8 10v4"/>
+                    <path d="M12 10v2"/>
+                    <path d="M16 10v6"/>
+                    </svg>
+                        <div className="space-y-2">
+                            <h3 className="font-bold">Management</h3>
+                            <p className="text-sm text-muted-foreground">
+                                Free and paid subscriptions using Stripe. (To be added.)
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section></>
+    );
+  };
